84cc324f4aac5e6c8ddfda516564d0cfe87b83b9
Xinran Che <gracexinran@gmail.com>
2019-05-07 20:49:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,8 +45,11 @@ def winner(board)
     index_2 = win[1]
     index_3 = win[2]
     if board[index_1] == "X" && board[index_2] == "X" && board[index_3] == "X"
-      return 
+      return "x"
     elsif board[index_1] == "O" && board[index_2] == "O" && board[index_3] == "O"
-      return win
+      return "O"
     end
+  end
+  return "nil"
+end
   
